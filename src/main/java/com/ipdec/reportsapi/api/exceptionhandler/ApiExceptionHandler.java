2c59b76d247364bd8253ea5d11e2a43d05894128
Francisco <francisco.junior@ipdec.org>
2023-07-26 17:07:51 -0400

    fix das classes de exception

--- src/main/java/com/ipdec/reportsapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/ipdec/reportsapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -52,9 +52,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         var campos = new ArrayList<Problema.Campo>();
-        for(ObjectError error : ex.getBindingResult().getAllErrors()){
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
 
-            String nome = ((FieldError)error).getField();
+            String nome = ((FieldError) error).getField();
             String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 
             campos.add(new Problema.Campo(nome, mensagem));
@@ -63,7 +63,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         var problema = new Problema();
         problema.setStatus(status.value());
         problema.setTitulo("Um ou mais campos estão inválidos. "
-                +"Faça o preenchimento correto e tente novamente");
+                + "Faça o preenchimento correto e tente novamente");
         problema.setDataHora(OffsetDateTime.now().withNano(0));
         problema.setCampos(campos);
 
--- src/main/java/com/ipdec/reportsapi/api/exceptionhandler/exception/NegocioException.java
+++ src/main/java/com/ipdec/reportsapi/api/exceptionhandler/exception/NegocioException.java
@@ -1,6 +1,9 @@
 package com.ipdec.reportsapi.api.exceptionhandler.exception;
 
-public class NegocioException extends RuntimeException{
+import java.io.Serial;
+
+public class NegocioException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public NegocioException(String message) {
