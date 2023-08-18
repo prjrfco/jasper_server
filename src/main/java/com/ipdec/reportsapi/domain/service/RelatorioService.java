d1ca072be60045e491a9dde7f7bf720559c787b9
Francisco <francisco.junior@ipdec.org>
2023-08-18 15:41:21 -0400

    removido fila para envio de relatorios para apis clientes

--- src/main/java/com/ipdec/reportsapi/domain/service/RelatorioService.java
+++ src/main/java/com/ipdec/reportsapi/domain/service/RelatorioService.java
@@ -3,13 +3,10 @@ package com.ipdec.reportsapi.domain.service;
 import com.ipdec.reportsapi.api.dto.RelatorioDto;
 import com.ipdec.reportsapi.api.exceptionhandler.exception.EntidadeNaoEncontradaException;
 import com.ipdec.reportsapi.config.feignService.ApiService;
-import com.ipdec.reportsapi.config.feignService.SendRelatorioDto;
 import com.ipdec.reportsapi.domain.model.Backend;
-import com.ipdec.reportsapi.domain.model.FilaEnvio;
 import com.ipdec.reportsapi.domain.model.Historico;
 import com.ipdec.reportsapi.domain.model.Relatorio;
 import com.ipdec.reportsapi.domain.repository.BackendRepository;
-import com.ipdec.reportsapi.domain.repository.FilaEnvioRepository;
 import com.ipdec.reportsapi.domain.repository.RelatorioRepository;
 import feign.Feign;
 import feign.Target;
@@ -24,8 +21,10 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 @Import(FeignClientsConfiguration.class)
@@ -36,9 +35,6 @@ public class RelatorioService {
 
     @Autowired
     private BackendRepository backendRepository;
-
-    @Autowired
-    private FilaEnvioRepository filaEnvioRepository;
 
     ApiService apiService;
 
@@ -77,13 +73,6 @@ public class RelatorioService {
 
         relatorio = repository.save(relatorio);
 
-        try {
-            this.apiService.sendReport(new URI(backend.getUrl()), new SendRelatorioDto(relatorio));
-        } catch (Exception e) {
-            filaEnvioRepository.save(new FilaEnvio(relatorio));
-            e.printStackTrace();
-        }
-
         return new RelatorioDto(relatorio);
     }
 
@@ -102,26 +91,6 @@ public class RelatorioService {
         relatorio.setVersao(relatorio.getVersao() + 1);
 
         relatorio = repository.save(relatorio);
-
-        Optional<FilaEnvio> presente = filaEnvioRepository.findByRelatorioId(relatorio.getId());
-
-        boolean erro = false;
-        try {
-            this.apiService.sendReport(new URI(relatorio.getBackend().getUrl()), new SendRelatorioDto(relatorio));
-        } catch (Exception e) {
-            erro = true;
-            if (presente.isEmpty()) {
-                presente = Optional.of(new FilaEnvio(relatorio));
-            } else {
-                presente.get().setCriadoEm(new Date());
-            }
-            filaEnvioRepository.save(presente.get());
-            e.printStackTrace();
-        }
-
-        if (presente.isPresent() && !erro) {
-            filaEnvioRepository.delete(presente.get());
-        }
 
         return new RelatorioDto(relatorio);
     }
